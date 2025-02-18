61954a380e4043d9522045663646b6a8c71ab7db
Augusto Henrique <aaugustohenrique05@gmail.com>
2025-02-18 11:31:54 -0300

    innit: criação das classes Afd e State, criação do métodos process

---
+++ Afd.java
@@ -0,0 +1,49 @@
+import java.util HashMap;
+
+public class Afd
+{
+    HashMap<State,String> states;
+    StringBuilder alphabet;
+    State initial;
+    HashMap<State, String> finalStates;
+    StringBuilder word;
+    public Afd
+    {
+        states = new HashMap<>();
+        alphabet = new StringBuilder();
+        finalStates = new HashMap<>();
+
+    }
+
+    public void process(String word)
+    {
+        word = new StringBuilder(word);
+
+        if(!)
+        for(char symbol: word)
+        {
+
+        }
+
+
+
+
+
+    }
+
+    private boolean isWord(word)
+    {
+        for(String symbol: alphabet)
+        {
+            for(String word: word)
+            {
+                
+            }
+        }
+
+    }
+
+
+
+
+}
---
+++ State.java
@@ -0,0 +1,27 @@
+public class State
+{
+    HashMap<String, State> states;
+    public State
+    {
+
+    }
+
+    public setAffordableStates(State state, String name)
+    {
+        states.put(name, state);
+    }
+
+    public setTransition(String alphapbet, State)
+    {
+
+    }
+
+    public State δ(State state, String Σ)
+    {
+
+    }
+
+
+
+
+}
