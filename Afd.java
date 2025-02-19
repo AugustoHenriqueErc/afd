70d3d07a76c721d16cd1bd9bedbac0e03cb3ac67
Augusto Henrique <aaugustohenrique05@gmail.com>
2025-02-19 10:25:38 -0300

    Alterações em tipo de coleção, criação de métodos set, implementaçao de charArray no método process

--- Afd.java
+++ Afd.java
@@ -1,33 +1,54 @@
+import java.util ArraList;
 import java.util HashMap;
 
 public class Afd
 {
-    HashMap<State,String> states;
-    StringBuilder alphabet;
-    State initial;
-    HashMap<State, String> finalStates;
-    StringBuilder word;
-    public Afd
+    ArrayList<State> states;
+    ArrayList<String> alphabet;
+    State initialState;
+    ArrayList<State> finalStates;
+    String word;
+    public Afd(State initialState)
     {
-        states = new HashMap<>();
-        alphabet = new StringBuilder();
-        finalStates = new HashMap<>();
+        states = new ArrayList<>();
+        alphabet = new ArrayList<>();
+        finalStates = new ArrayList<>();
+    }
+
+    public void setAlphabet(String symbol)
+    {
+        alphabet.add(symbol);
+    }
+
+    public void setStates(State state)
+    {
+        states.add(state);
+    }
+
+    public void setFinalStates(State finalState)
+    {
+        for(State state: states)
+        {
+            if(state == finalState) finalStates.add(state);
+            else throw new IllegalArgumentException("Afd does not contain this state");
+        }
 
     }
+
+
 
     public void process(String word)
     {
         State actualState = initial;
-        word = new StringBuilder(word);
+        word = word.toCharArray();
         for(int i = 0; i < word.lenght();i++)
         {
-            if(isSymbol(word.get(i))!= -1) actualState.δ(word.get(i)) = actualState;
         }
     }
 
-    private int isSymbol(String Symbol)
+    private int isSymbol(String symbol)
     {
-        return word.indexOf(Symbol);
+
     }
 
 
